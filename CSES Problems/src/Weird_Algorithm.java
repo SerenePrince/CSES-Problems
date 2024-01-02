7416b903f4166b667695f9ad17be592b6f0d7544
Noah Park-Nguyen <park0597@algonquinlive.com>
2024-01-02 01:43:41 -0500

    Create Source

---
+++ CSES Problems/src/Weird_Algorithm.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Weird_Algorithm {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		long n = Long.parseLong(br.readLine().trim());
+		System.out.print(n);
+		while (n > 1) {
+			if (n % 2 == 0) {
+				n /= 2;
+			} else {
+				n = n * 3 + 1;
+			}
+			System.out.printf(" %d", n);
+		}
+		br.close();
+	}
+}
