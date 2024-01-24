0709d572e0b879d67f85779a6defd2dcdfc59520
Noah Park-Nguyen <noahparknguyen@gmail.com>
2024-01-24 13:19:21 -0500

    Add Solution

--- Introductory Problems/Repetitions.java
+++ Introductory Problems/Repetitions.java
@@ -1,4 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Repetitions {
-
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String sequence = br.readLine();
+		int count = 1, result = 1;
+		char previous = sequence.charAt(0);
+		for (int i = 1; i < sequence.length(); i++) {
+			if (sequence.charAt(i) == previous) {
+				count++;
+			} else {
+				count = 1;
+				previous = sequence.charAt(i);
+			}
+			if (count > result) {
+				result = count;
+			}
+		}
+		System.out.println(result);
+	}
 }
